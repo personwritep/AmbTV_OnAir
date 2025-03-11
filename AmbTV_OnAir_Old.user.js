1980372ac95fce47b50e0eabd8f4e7f1a95b08d6
Personwritep <89448434+personwritep@users.noreply.github.com>
2025-03-11 20:40:45 +0900

    Update AmbTV_OnAir_Old.user.js

--- AmbTV_OnAir_Old.user.js
+++ AmbTV_OnAir_Old.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        AmbTV OnAir
+// @name        AmbTV OnAir Old
 // @namespace        http://tampermonkey.net/
 // @version        2.3
 // @description        AbemaTV ユーティリティ
